2be5f90eaa385061fb4fdf546efc34fc8cddcbd7
Kim HyunWoo <stories2@naver.com>
2022-08-06 22:28:03 +0900

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello Docker World!?!!211!";
+	@GetMapping
+	public String hello() {
+		return "Hell World";
 	}
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
