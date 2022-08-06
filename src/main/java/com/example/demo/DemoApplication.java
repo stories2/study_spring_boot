945cb7c7221d1cc033d7082f4cdfdec91279f631
Kim HyunWoo <stories2@naver.com>
2022-08-06 22:49:13 +0900

    API Layer

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,19 +12,7 @@ import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
-
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(new Student(
-				1L,
-				"Mariam",
-				"mariam.jjamal@gmail.com",
-				LocalDate.of(2000, Month.JULY, 5),
-				21
-		));
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
