d4d64037035dd505f9a7334f2daa5843a7845d89
Kim HyunWoo <stories2@naver.com>
2022-08-06 22:41:51 +0900

    student obj model

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -13,8 +16,14 @@ import java.util.List;
 public class DemoApplication {
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
+	public List<Student> hello() {
+		return List.of(new Student(
+				1L,
+				"Mariam",
+				"mariam.jjamal@gmail.com",
+				LocalDate.of(2000, Month.JULY, 5),
+				21
+		));
 	}
 
 	public static void main(String[] args) {
