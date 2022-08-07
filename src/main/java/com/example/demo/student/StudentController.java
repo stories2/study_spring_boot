5cca691ce6e4fbac1b8357d3989cd7b86ccf313b
Kim HyunWoo <stories2@naver.com>
2022-08-07 15:27:54 +0900

    service layer

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +13,14 @@ import java.util.List;
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    @Autowired //Dependency Injection
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
     @GetMapping
     public List<Student> getStudents() {
-        return List.of(new Student(
-                1L,
-                "Mariam",
-                "mariam.jjamal@gmail.com",
-                LocalDate.of(2000, Month.JULY, 5),
-                21
-        ));
+        return this.studentService.getStudents();
     }
 }
