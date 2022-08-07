862d4d4b8172303f3ebc54a0aab32ed5fb1dc58b
Kim HyunWoo <stories2@naver.com>
2022-08-07 17:36:10 +0900

    email finder

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,11 +1,18 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 // For JPA
 // Object type, ID type
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+//    Select * from student where email = ?;
+//    JPQL
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
