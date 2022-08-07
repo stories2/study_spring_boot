87a6057dba7ae8b65983a25f1ce755f4529ce840
Kim HyunWoo <stories2@naver.com>
2022-08-07 16:27:27 +0900

    Create StudentConfig.java

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student stories2 = new Student(
+                    1L,
+                    "stories2",
+                    "stories2@gapmoe.net",
+                    LocalDate.of(2000, Month.JULY, 5),
+                    21
+            );
+
+            Student hazimenya = new Student(
+                    "hazimenya",
+                    "hazimenya@gapmoe.net",
+                    LocalDate.of(2022, Month.AUGUST, 21),
+                    22
+            );
+
+            studentRepository.saveAll(
+                    List.of(stories2, hazimenya)
+            );
+        };
+    }
+}
