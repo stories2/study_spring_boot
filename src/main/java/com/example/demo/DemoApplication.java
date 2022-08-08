006027807869a186c84540f4e60ecb2fc701cb7a
Kim, Hyunwoo | RSK <hyunwoo.kim@rakuten.com>
2022-08-08 16:42:32 +0900

    kafka

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,7 +14,7 @@ import java.util.List;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[]  args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
